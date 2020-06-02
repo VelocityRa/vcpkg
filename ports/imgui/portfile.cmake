712458b1b5514879d01e12cc580f9122d221c3bd
Nick Renieris <velocityra@gmail.com>
2020-06-02 13:49:24 +0300

    [imgui] use VelocityRa/docking-tables

--- ports/imgui/portfile.cmake
+++ ports/imgui/portfile.cmake
@@ -2,10 +2,10 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO ocornut/imgui
-    REF 5116eee108986ba6f41eea6f8759eb6acc042670 # docking as of 05-05-20
-    SHA512 17e6da59dcf867454babfd54a880deed32027e4a25cd386c267686181795037036e2ada3d22bd482541cd595b3af3ada357c3079d3e1615565f50f1d029720ed
-    HEAD_REF docking
+    REPO VelocityRa/imgui
+    REF d19845c9ca362ccbbe371eaa26b2867854ff8800 # docking-tables as of 30-05-20
+    SHA512 e2816ef9b8f367f3159a6d24fefb98aac47855e55d2641b40ee4c579263a58e7892542ceb4c786e5f1e61f2e4a4286c4e1ac4ba012446c64b495c73747ffb7c5
+    HEAD_REF docking-tables
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
