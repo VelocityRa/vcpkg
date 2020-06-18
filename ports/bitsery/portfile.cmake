bf5b2376733cd04aa30e4dda3cbe600f31baeaad
Rémy Tassoux <contact@rt2.fr>
2020-06-19 01:26:36 +0200

    [bitsery] Update to 5.1.0 version (#11912)
    * [bitsery] Update to 5.1.0 version
    
    * [bitsery] Remove unused patch

--- ports/bitsery/portfile.cmake
+++ ports/bitsery/portfile.cmake
@@ -4,7 +4,6 @@ vcpkg_from_github(
 	REF 7efcf8b6e85c4e887042bd78d2e0891dad25cd30 # kl branch
 	SHA512 0aa378aee247a29011ca1a4bd1fc2559d97f35ed48ea6633cab5cb86ca10970389b8af785272cc7edb97aa7bea0ec1d72034c59c434a4abdd02255c3027affa3
 	HEAD_REF master
-	PATCHES fix-install-paths.patch
 )
 
 vcpkg_configure_cmake(
